8bc6895b4d2f2b661ac94ffa48e478b93ff0049e
NinoWangy <101724094+NinoWangy@users.noreply.github.com>
2022-03-17 16:28:57 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // self or public
-global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+global.self = true //jadiin true klo gk mau fitur bot lu di pke sama org lain
 global.anticall = false
 global.anti_delete = false
 
